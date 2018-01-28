0433352f1770c7a97a3dcccf70b7e6617286c759
Joe Pasquantonio <joe.pasquantonio@gmail.com>
2018-01-28 10:41:52 -0500

    added gameover, day limit, fixed event display to show the whole day correctly. player attributes updating when event occurs, new day starts with sleep now.

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import argparse  # maybe later
 from curses import KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT
 from random import randint
 
+
 class Player:
     """
     Choose your character (literally your 'char' ex. '@', '0', '#')
@@ -21,17 +22,19 @@ class Player:
     -- maybe luck could be fun
     """
 
-    def __init__(self, y, x, dimensions, wealth, knowledge, strength):
+    def __init__(self, y, x, dimensions, money, knowledge, strength):
         self.y = y
         self.x = x
         self.dimensions = dimensions
-        self.wealth = wealth
+        self.money = money
         self.knowledge = knowledge
         self.strength = strength
         self.character = '@'
         self.name = "tab"
         self.in_building = False
         self.building = None
+        self.employed = False
+        self.wage = 8
 
     def move(self, key):
         if key == KEY_RIGHT and self.x < self.dimensions[1] - 2:
@@ -43,6 +46,17 @@ class Player:
         if key == KEY_DOWN and self.y < self.dimensions[0] - 1:
             self.y = self.y + 1
         return self.y, self.x
+
+    def manage_event(self, event):
+        if event == "c":  # class
+            self.knowledge += 2
+            self.money -= 20
+        if event == "e":  # exercise
+            self.strength += 1
+        if event == "s":  # study
+            self.knowledge += 1
+        if event == "w":  # work
+            self.money += (self.wage * 4)  # shifts are 4 hours long
 
 
 class Citizen:
@@ -69,8 +83,8 @@ class Information:
         self.price_list = ["", "", "", ""]
 
     def __str__(self):
-        return "Wealth: {} Knowledge: {} Strength: {}".format(
-                                                        self.player.wealth,
+        return "money: {} Knowledge: {} Strength: {}".format(
+                                                        self.player.money,
                                                         self.player.knowledge,
                                                         self.player.strength)
 
@@ -120,38 +134,73 @@ class Building:
             string += " {}: {}\n".format(count, option)
         return string
 
+
 class Day:
     """
     A day consists of 24 hours.
 
     Events take up hours but earn you money or knowledge.
     """
-    def __init__(self):
+    def __init__(self, player):
+        self.player = player
         self.length = 24
+        self.display_day = []
         self.day = []
-        self.events = {"work": "w",
-                       "class": "c",
-                       "study": "s",
-                       "exercise": "e",
-                       "sleep": "z"}
+        self.events = {"work": ["w", 4],
+                       "class": ["c", 2],
+                       "study": ["s", 1],
+                       "exercise": ["e", 1],
+                       "sleep": ["z", 8]}
 
     def __str__(self):
+        return " ".join(self.display_day)
+
+    def real_day(self):
         return " ".join(self.day)
 
     def add_event(self, e):
         event = self.events[e]
-        self.day.append(event)
+        if event[1] + len(self.day) - 1 < self.length:
+            self.player.manage_event(event[0])
+            padding = self.display_day.index("_")
+            for i in xrange(0, event[1]):
+                self.display_day[padding+i] = event[0]
+                self.day.append(event[0])
+
+
+class DayManager:
+    """
+    A collection of days and advances days when necesary.
+    """
+    def __init__(self, player):
+        self.day_list = []
+        self.today = None
+        self.player = player
+
+    def add_day(self):
+        self.day_list.append(Day(self.player))
+        self.today = self.day_list[-1]
+        for i in xrange(0, 24):
+            self.today.display_day.append("_")
+
+        self.today.add_event("sleep")
+
+    def day_number(self):
+        return len(self.day_list)
 
 
 def format_time(start):
     return int(time.time() - start)
 
 
+def generate_map(dimensions):
+    pass
+
 def pregame():
-    wealth = randint(1, 10)
+    money = 100
     knowledge = randint(1, 10)
     strength = randint(1, 10)
-    return [wealth, knowledge, strength]
+    return [money, knowledge, strength]
 
 
 def redraw_world(world, buildings):
@@ -173,11 +222,8 @@ def game(rows, cols, y, x, stats):
     world.border(0)
     world.nodelay(1)
     world.addstr(0, 24, title)
-
-    # Initialize Day
-    today = Day()
+    max_days = 10
     bottom = 20
-    world.addstr(bottom, 1, 'Events: ['+str(today)+']')
     dimensions = world.getmaxyx()
     dimensions = (bottom, dimensions[1])
 
@@ -186,6 +232,10 @@ def game(rows, cols, y, x, stats):
     player_x = 1
     player = Player(player_y, player_x, dimensions,
                     stats[0], stats[1], stats[2])
+
+    # Initialize Day
+    day_manager = DayManager(player)
+    day_manager.add_day()
 
     # Initialize information
     info_y = 21
@@ -196,7 +246,7 @@ def game(rows, cols, y, x, stats):
 
     # Initialize buildings
     work = Building(15, 20, 4, "work", "W", ["work", "leave"])
-    school = Building(10, 30, 5, "school", "S", ["class", "study", "leave"])
+    school = Building(10, 30, 5, "school", "S", ["class", "leave"])
     gym = Building(5, 5, 3, "gym", "G", ["exercise", "leave"])
     house = Building(2, 48, 2, "house", "H", ["sleep", "leave"])
 
@@ -215,6 +265,7 @@ def game(rows, cols, y, x, stats):
 
     # Game Loop
     while key != ord('q'):
+        """
         # Timekeeping
         # This will be changed to event but... i think i can find a use for this
         seconds = format_time(start_time)
@@ -226,11 +277,13 @@ def game(rows, cols, y, x, stats):
             seconds = 0
             next_day = False
             day += 1
-        world.addstr(29, 0, "Day: {}".format(day))
+        """
+
+        world.addstr(29, 0, "Day: {}".format(day_manager.day_number()))
 
         # rewrite info on screen... probably inefficient but will do for now
         world.addstr(info.y, info.x, str(info))
-        world.addstr(bottom, 1, 'Events: ['+str(today)+']')
+        world.addstr(bottom, 1, 'Events: ['+str(day_manager.today)+']')
 
         # player movement
         key = world.getch()
@@ -245,23 +298,33 @@ def game(rows, cols, y, x, stats):
                     player.in_building = True
                     player.building = b
                     world.clear()
-                    world.addstr(0, 24, b.purpose)
                     world.addstr(1, 0, b.display_options())
                     world.border(0)
+                    world.addstr(0, 24, b.purpose)
                     player.x += 1
 
         # exit building and return to world
         if player.in_building:
-            if key == ord('0'):
+            if key == ord('0') and player.building.purpose != "house":  # do event
                 event = player.building.inside_options[0]
-                today.add_event(event)
+                day_manager.today.add_event(event)
+            if key == ord('0') and player.building.purpose == "house":
+                day_manager.add_day()
+                world.addstr(bottom, 1, 'Events: ['+str(day_manager.today)+']')
+            if key == ord('1'):  # exit building
                 player.in_building = False
                 redraw_world(world, buildings)
 
+        # Check if day limit has been reached
+        if day_manager.day_number() > max_days:
+            gameover = True
+            key = ord('q')
 
     curses.beep()
     curses.endwin()
     print "Gameover."
+    print "Knowledge: {}".format(player.knowledge)
+    print "Wealth: {}".format(player.money)
 
 
 if __name__ == "__main__":
